c02749f174e69c54e6feac0cce5fc19f163729b0
Prasad Nelaturi <prasadnelaturi333@gmail.com>
2026-07-04 17:00:45 +0530

    hero video 1.4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import YouTube from "react-youtube";
 import { Volume2, VolumeX } from "lucide-react";
 
